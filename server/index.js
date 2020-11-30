4798e87a2c2879d1d17a5cd1775e7ee6772f5e6a
dwaynediddy <burrows.dwayne@icloud.com>
2020-11-30 14:34:25 +1000

    backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
+const mysql = require('mysql')
 
-app.get("/", (req, res) => {
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'CRUDDatabase',
+})
+
+app.use(cors)
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/", (req, res) => {
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+
+    const sqlInsert = 'INSERT INTO movie_reviews (movie_reviews) VALA (?,?)'
+    db.query(sqlInsert, (movieName, movieReview), (err, result) => {
+        console.log(result)
+    })
     res.send("hello")
 })
 
